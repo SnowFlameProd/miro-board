2e4a7fef71013be77b1bb364ed2be3ae1662fab1
snowflame <e.lepikhov@lms-service.ru>
2025-05-28 17:37:14 +0300

    added config

--- src/features/boards-list/boards-list.page.tsx
+++ src/features/boards-list/boards-list.page.tsx
@@ -1,12 +1,13 @@
 import { href, Link } from "react-router-dom";
 import { ROUTES } from "@/shared/model/routes.tsx";
+import { CONFIG } from "@/shared/model/config.ts";
 
 function BoardsListPage() {
   return (
     <div>
-      <h1>Boards list</h1>
+      <h1>Boards list {CONFIG.API_BASE_URL}</h1>
 
-      <Link to={href(ROUTES.BOARD, {boardId: '1'})}>Board 1</Link>
+      <Link to={href(ROUTES.BOARD, { boardId: "1" })}>Board 1</Link>
     </div>
   );
 }
